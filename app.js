e09cc34f39517230f2c5d03c9d88d3e37acdece1
Shreyas Murali <shreyas2407@gmail.com>
2017-11-10 23:53:36 +0000

    4th commit

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var express = require("express");
 var app = express();
 var request = require("request");
-var mongoose = require("mongoose");
-
 
 
 
@@ -10,16 +8,9 @@ app.set("view engine","ejs");
 app.use(express.static("public"));
 
 
-var oldataSchema = new mongoose.Schema({
-    
-    date: String,
-    price: Number
-});
-
 
 var parseddata = {};
 var data = 0;
-var oldata=[] ;
 request("https://api.coindesk.com/v1/bpi/currentprice.json",function(error,response,body)
 {
     
@@ -30,18 +21,7 @@ request("https://api.coindesk.com/v1/bpi/currentprice.json",function(error,respo
     
 }
 });
-request("https://api.coindesk.com/v1/bpi/historical/close.json",function(error,response,body)
-{
-    
-    if(!error && response.statusCode==200)
-    {parseddata=(JSON.parse(body));
-     oldata = (parseddata["bpi"]);
-     console.log("yestfirst"+oldata);
-     console.log(JSON.stringify(oldata));
-     console.log(oldata);
-    
-}
-});
+
 
 
 
@@ -50,21 +30,12 @@ app.get("/",function(req,res)
   
     request("https://api.coindesk.com/v1/bpi/currentprice.json",function(error,response,body)
     {
-          request("https://api.coindesk.com/v1/bpi/historical/close.json",function(error,response,body)
-                {
-                    
-                    if(!error && response.statusCode==200)
-                    { parseddata=(JSON.parse(body));
-                      oldata= (parseddata["bpi"]);
-                      console.log("yesterday"+parseddata["bpi"]);
-                    
-                      }
-                });
+         
                 if(!error && response.statusCode==200)
                     {   parseddata=(JSON.parse(body));
                         data= (parseddata["bpi"]["USD"]["rate"]);
                         console.log("new",parseddata["bpi"]["USD"]["rate_float"]);
-                        res.render("index",{data:data,oldata:oldata});
+                        res.render("index",{data:data});
                     }
                 });
  
